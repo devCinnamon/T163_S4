58809a1ae5ea60fc34c242f5eacd174a2f4f1e6a
devCinnamon <concof.zp@gmail.com>
2022-04-22 02:59:40 -0400

    Added Final Projects.

--- GAME ENGINES II/Lab1_Gastello/Source/Lab1_Gastello/Lab1_Gastello.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Lab1_Gastello.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lab1_Gastello, "Lab1_Gastello" );
